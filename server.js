c925050365f232c6473c1045bf1fb3f13997074e
Duy Dinh <dinhquangduynt@gmail.com>
2020-06-28 15:05:45 +0700

    c

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/<name-on-package.json>'));
 
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
-);
-});
+// app.get('/*', function(req, res) {
+//   res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
+// );
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
